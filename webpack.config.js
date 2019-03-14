5059302c61bc1e116acbbdcfd4c6ebc4fbc79299
abhijet <abhijet.pokhrel@gmail.com>
2019-03-14 11:44:16 +0545

    fixed dist location

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   entry: './src/browser.js',
   output: {
-    filename: './dist/AIJAS.js',
+    filename: './AIJAS.js',
     path: path.resolve(__dirname, 'dist'),
   }
 };
